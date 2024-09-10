d9397026e7aa3657147253056ade9f8cded35fed
thirstyice <thirstyice@users.noreply.github.com>
2024-09-09 19:06:28 -0600

    Make example more generic

--- examples/RedEyeLoopback/RedEyeLoopback.ino
+++ examples/RedEyeLoopback/RedEyeLoopback.ino
@@ -3,12 +3,22 @@
 
 char toPrint = 'A';
 
+#ifndef REDEYE_RX_PIN
+	#define REDEYE_RX_PIN 2
+#endif
+#ifndef REDEYE_TX_PIN
+	#define REDEYE_TX_PIN 3
+#endif
+#ifndef REDEYE_INVERTED
+	#define REDEYE_INVERTED false
+#endif
+
 void setup() {
-	pinMode(11, OUTPUT);
-	pinMode(2, INPUT_PULLUP);
+	pinMode(REDEYE_TX_PIN, OUTPUT);
+	pinMode(REDEYE_RX_PIN, INPUT_PULLUP);
 	Serial.begin(115200);
-	RedEye.begin(2,11, false, false);
-	RedEye.setMode(redeye::ModeDuplex);
+	RedEye.begin(REDEYE_RX_PIN,REDEYE_TX_PIN, REDEYE_INVERTED, REDEYE_INVERTED);
+	RedEye.setMode(RedEye.ModeDuplex);
 	while (!Serial);
 	Serial.println("Ready!");
 }
